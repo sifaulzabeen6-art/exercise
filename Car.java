c5f9614a2bafde50b62131ffc345a16b4cd43f2f
sifaulzabeen6-art <sifaulzabeen6@gmail.com>
2026-02-26 20:06:49 +0530

    hello

---
+++ Car.java
@@ -0,0 +1,21 @@
+class Car 
+{
+	public static void main(String[]args)
+	{
+		String carname="Tesla model s";
+		int milage=396;
+		String fueltype="Electric";
+		float price =85000.00f;
+		String brand="Tesla";
+		System.out.println(carname);
+		System.out.println(milage);
+		System.out.println(fueltype);
+		System.out.println(price);
+		System.out.println(brand);
+		
+		
+		
+		
+	
+	}
+}
