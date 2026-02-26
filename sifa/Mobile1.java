020f36d3a7afa5f05304d68a5467221531eb39df
sifaulzabeen6-art <sifaulzabeen6@gmail.com>
2026-02-26 19:30:03 +0530

    hello

---
+++ sifa/Mobile1.java
@@ -0,0 +1,20 @@
+class Mobile1
+{
+	public static void main(String [] args)
+	{
+		String mobilename="Galaxys24ultra";
+		String mobilebrand="samsung";
+		int batterycapacity=5000;
+		float screensize=6.8f;
+		float price=74000.58f;
+		System.out.println(mobilename);
+		System.out.println(mobilebrand);
+		System.out.println(batterycapacity);
+		System.out.println(screensize);
+		System.out.println(price);
+		
+	}
+	
+	
+	
+}
