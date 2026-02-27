8d9273dcd0bc4b3ed724000b254ba0993c393cf1
sifaulzabeen6-art <sifaulzabeen6@gmail.com>
2026-02-27 09:48:00 +0530

    hello

---
+++ Product.java
@@ -0,0 +1,21 @@
+class Product
+{
+	public static void main(String[]args)
+	{
+		int storesproductid;
+		String productname;
+		float productprice;
+		float productrating;
+		boolean productavailability;
+		storesproductid=301;
+		productname="Laptop";
+		productprice=55000.99f;
+		productrating=4.5f;
+		productavailability=true;
+		System.out.println(storesproductid);
+		System.out.println(productname);
+		System.out.println(productprice);
+		System.out.println(productrating);
+		System.out.println(productavailability);
+	}
+}
