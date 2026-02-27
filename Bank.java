8b7590f90544b7a0c8606fbb696b1adb0f163992
sifaulzabeen6-art <sifaulzabeen6@gmail.com>
2026-02-27 09:30:39 +0530

    hello

---
+++ Bank.java
@@ -0,0 +1,19 @@
+class Bank
+{
+	public static void main(String[]args)
+	{
+		long accountnumber;
+		String accountholdername;
+		float accountbalance;
+		String accounttype;
+		accountnumber=12345678901l;
+		accountholdername="Anitha";
+		accountbalance=25000.5f;
+		accounttype="saving";
+		System.out.println(accountnumber);
+		System.out.println(accountholdername);
+		System.out.println(accountbalance);
+		System.out.println(accounttype);
+		
+	}
+}
