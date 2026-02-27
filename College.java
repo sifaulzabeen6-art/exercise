1c9f5d76a32072b4c8d801847e998c32c95c7ca4
sifaulzabeen6-art <sifaulzabeen6@gmail.com>
2026-02-27 09:39:55 +0530

    hello

---
+++ College.java
@@ -0,0 +1,19 @@
+class College
+{
+	public static void main(String[]args)
+	{
+		int registernumber;
+		String studentname;
+		String coursename;
+		float fees;
+		registernumber=2023123;
+		studentname="karthik";
+		coursename="B.Tech";
+		fees=75000.0f;
+	    System.out.println(registernumber);
+        System.out.println(studentname);
+		System.out.println(coursename);
+        System.out.println(fees);
+	
+	}
+}
