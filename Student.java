34db8f3bf52875a6f67899527b1022c38bf4f6fc
sifaulzabeen6-art <sifaulzabeen6@gmail.com>
2026-02-27 09:14:03 +0530

    hello

---
+++ Student.java
@@ -0,0 +1,21 @@
+class Student
+{
+	public static void main(String[]args)
+	{
+		int studentid;
+		String name;
+		int age;
+		float percentage;
+		char grade;
+		studentid=101;
+		name="saranya";
+		age=20;
+		percentage=85.6f;
+		grade='A';
+		System.out.println(studentid);
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(percentage);
+        System.out.println(grade);
+	}
+}
