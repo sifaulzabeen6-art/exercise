d624a0fb2a527108aecae74639630152f8fe9acf
sifaulzabeen6-art <sifaulzabeen6@gmail.com>
2026-02-27 09:23:09 +0530

    hello

---
+++ Employee.java
@@ -0,0 +1,21 @@
+class Employee
+{
+	public static void main(String[]args)
+	{
+		int employeeid;
+		String employeename;
+		float salary;
+		String department;
+		boolean permanentemployee;
+		employeeid=5001;
+		employeename="ravi";
+		salary=45000.75f;
+		department="it";
+		permanentemployee=true;
+		System.out.println(employeeid);
+		System.out.println(employeename);
+		System.out.println(salary);
+		System.out.println(department);
+		System.out.println(permanentemployee);
+	}
+}
