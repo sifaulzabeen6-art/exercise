7e718019cddeed6aca9070cd19734495c2c50e1e
sifaulzabeen6-art <sifaulzabeen6@gmail.com>
2026-02-28 15:17:39 +0530

    hello

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+class Calculator
+{
+	public static void main(String[]args)
+	{
+		int a=10;
+		int b=50;
+		int c=a+b;
+		int d=a-b;
+		int e=a*b;
+		int f=a/b;
+		int g=a%b;
+		System.out.println("Addition:"+c);
+		System.out.println("subtraction"+d);
+		System.out.println("multiplication"+e);
+		System.out.println("quotient"+f);
+		System.out.println("remainder"+g);
+		
+		
+	}
+}
