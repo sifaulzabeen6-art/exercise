cbfcd3a4b4ecec668f9fb8092930047ca966d9f3
sifaulzabeen6-art <sifaulzabeen6@gmail.com>
2026-02-28 15:29:26 +0530

    hello

---
+++ Relation.java
@@ -0,0 +1,18 @@
+class Relation
+{
+	public static void main(String[]args)
+	{
+		int a=10;
+		int b=20;
+		boolean c=a==b;
+		boolean d=a>b;
+		boolean e=a<b;
+		boolean f=a>=b;
+		boolean g=a<=b;
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+	}
+}
