ced081620099e3b7446f370ba47d738376459619
sifaulzabeen6-art <sifaulzabeen6@gmail.com>
2026-03-03 13:54:13 +0530

    hello

---
+++ Mixed.java
@@ -0,0 +1,19 @@
+class Mixed
+{
+	public static void main(String[]args)
+	{
+		int a=10;
+		int b=5;
+		int c=15;
+		int d=a+b+c;
+		boolean e=a>b;
+		boolean f=b>a;
+		System.out.println("addition:"+d);
+		System.out.println("relation:"+e);
+		System.out.println("relation 2:"+f);
+		System.out.println("logical:"+(a>b&&c>a));
+		
+		
+		
+	}
+}
