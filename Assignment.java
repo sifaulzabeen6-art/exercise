2cafec55d62176ab87621f9b13e12eb5f74e5019
sifaulzabeen6-art <sifaulzabeen6@gmail.com>
2026-03-03 13:23:25 +0530

    hello

---
+++ Assignment.java
@@ -0,0 +1,17 @@
+class Assignment
+{
+	public static void main(String[]args)
+	{
+		int a=10;
+		a+=5;
+		System.out.println("the addition:"+a);
+		a-=5;
+		System.out.println("the subtraction:"+a);
+		a*=5;
+		System.out.println("the multiplication:"+a);
+		a/=5;
+		System.out.println("the quotient:"+a);
+		a%=5;
+		System.out.println("the remainder:"+a);
+	}
+}
