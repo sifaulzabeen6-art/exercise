ac50028d0d7ae6f7b242bf2713d153646c2e9c42
sifaulzabeen6-art <sifaulzabeen6@gmail.com>
2026-03-03 13:44:04 +0530

    hello

---
+++ Logical.java
@@ -0,0 +1,15 @@
+class Logical
+{
+	public static void main(String[]args)
+	{
+		int a=10;
+		int b=5;
+		boolean value=true;
+	
+		System.out.println("And operator:"+(a>b&&a<20));
+		System.out.println("or operator:"+(a<b||a>b));
+		System.out.println(!value);
+		
+		
+	}
+}
--- Relation.java
+++ Relation.java
@@ -9,10 +9,12 @@ class Relation
 		boolean e=a<b;
 		boolean f=a>=b;
 		boolean g=a<=b;
+		boolean h=a!=b;
 		System.out.println(c);
 		System.out.println(d);
 		System.out.println(e);
 		System.out.println(f);
 		System.out.println(g);
+		System.out.println(h);
 	}
 }
